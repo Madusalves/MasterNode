2a1f09a3a0301735b112b49e25cbce8f0cfcf92e
Madusalves <malvesduarte444@gmail.com>
2025-04-23 14:58:20 -0300

    refactor: enhance comments and structure in app.js and layout files

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const { engine } = require('express-handlebars'); // Importing the engine from express-handlebars
 
 const app = express();
 
-// Configurando o Handlebars
+// Handlebars
 app.engine('hbs', engine({
     layoutsDir: 'views/layouts/',
     defaultLayout: 'main-layout',
-    extname: 'hbs',
-    partialsDir: 'views/'
+    extname: 'hbs',         // File extension for Handlebars
+    partialsDir: 'views/'   // Directory for partials (small reusable templates)
 }));
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); // Setting the view engine to Handlebars
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminData = require('./routes/admin'); // Importing admin routes
 const shopRoutes = require('./routes/shop');
 
-// Middleware para processar o corpo da requisição
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // Middleware to parse URL-encoded data
 
-// Servindo arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
+app.use(express.static(path.join(__dirname, 'public'))); // Middleware to serve static files from the public directory
 
-// Tratamento de erros 404
-app.use((req, res, next) => {
+app.use('/admin', adminData.routes); // Mounting admin routes under /admin
+app.use(shopRoutes); // Mounting shop routes
+
+app.use((req, res, next) => {       // Middleware to handle 404 errors
     res.status(404).render('404', { 
         pageTitle: 'Page Not Found',
         path: '/404'
     });
 });
 
-app.listen(3000);
+app.listen(3000); 
