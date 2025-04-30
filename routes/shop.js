b4139aa4c091cd3c894e6ea976f0b0e43705c027
Madusalves <malvesduarte444@gmail.com>
2025-04-30 00:11:51 -0300

    refactor: improve comments and clean up layout in shop.js and main-layout.hbs

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const adminData = require('./admin');
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => { // standard route for handle responds to HTTP GET requests
   const products = adminData.products;
   res.render('shop', {    
     prods: products,
